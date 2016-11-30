13c90984108bd04ab995098a2702846c53d12bc1
xingjianhong <jianhong.xing@hunteron.com>
2016-11-30 17:01:54 +0800

    代码优化

--- crm-web/src/main/java/com/crm/controller/UserController.java
+++ crm-web/src/main/java/com/crm/controller/UserController.java
@@ -3,6 +3,7 @@ package com.crm.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.crm.service.IUserService;
@@ -14,7 +15,7 @@ public class UserController {
     private IUserService userService;
 
     @ResponseBody
-    @RequestMapping("test")
+    @RequestMapping(value = "test", method = RequestMethod.GET)
     public String getUser() {
         return userService.getUserName(115);
     }
