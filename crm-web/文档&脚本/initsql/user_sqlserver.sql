4a1cad9f842e1ae3053dfe97f9c5a2307ef12069
jianwufeng <x_jianhong@163.com>
2018-11-16 14:17:21 +0800

    初始化sql

---
+++ crm-web/文档&脚本/initsql/user_sqlserver.sql
@@ -0,0 +1,2 @@
+INSERT INTO [dbo].[t_user] ([id], [user_name], [email], [password], [create_time], [update_time]) VALUES (1, 'admin', 'admin@163.com', '1234', '2018-11-7 20:34:26', '2018-11-7 20:34:29');
+GO
